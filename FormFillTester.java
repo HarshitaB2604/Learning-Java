da7cd1789eddbfa088aa0d48a8ff48144390d396
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-06-18 21:07:28 -0700

    Create FormFillTester.java

---
+++ FormFillTester.java
@@ -0,0 +1,19 @@
+/*
+* uses information provided as parameters 
+* And returns the information formated 
+* The program will then print the infomation
+*/
+public class FormFillTester
+{
+    public static void main(String[] args)
+    {
+        FormFill filler = new FormFill("Karel", "Dog");
+        filler.setAddress(123, "Cherry Lane", "4B");
+        
+        System.out.println(filler.fullName());
+        System.out.println(filler.streetAddress());
+        
+        System.out.println(filler.creditCardInfo(123456789, 10, 2025));
+        
+    }
+}
